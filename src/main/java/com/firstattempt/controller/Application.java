8832895b1c68ce16163f78ca763ea364cee5faaf
Huy Nguyen <dshk127mm@gmail.com>
2026-02-13 01:58:52 -0500

    First step as this lol

---
+++ src/main/java/com/firstattempt/controller/Application.java
@@ -0,0 +1,40 @@
+package com.firstattempt.controller;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+/*
+Bean: Basically standard Java class that follows specific conventions, kinda like the RAII pattern for C++
+1. All private properties (use getters/setters)
+2. A public no-arg constructor
+3. Implements Serializable, meaning the objects can be written to streams, and hence files, object databases, etc.
+*/
+
+/*
+* I think this is the main method for the whole thing.
+* */
+@SpringBootApplication // combines the annotations of Configuration, EnableAutoConfiguration, and ComponentScan
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Them beans provided by SpringBoot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
+}
---
+++ src/main/java/com/firstattempt/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.firstattempt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+Annotation, it's for the compiler. 
+*/
+/*
+RestController: Denote that the class can be used by Spring MVC to handle web requests
+*/
+@RestController
+public class HelloController {
+    
+    /*
+    maps "/" to the index() method. wut.
+    */
+    @GetMapping("/")
+    public String index() {
+        return "Henlo. Za Warudo is a kut-ta.\n";
+    }
+}
