83832082390cd17955b83c485d69575560705fc1
Huy Nguyen <dshk127mm@gmail.com>
2026-02-20 01:48:02 -0500

    Post Controller basic REST responses will now return proper ResponseEntity objects

--- src/main/java/com/firstattempt/wack/post/PostController.java
+++ src/main/java/com/firstattempt/wack/post/PostController.java
@@ -1,5 +1,8 @@
 package com.firstattempt.wack.post;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -7,22 +10,22 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
 
     @PostMapping
-    public String createPost() {
-        return "Post created";
+    public ResponseEntity<String> createPost() {
+        return ResponseEntity.status(HttpStatus.CREATED).body("Post created");
     }
 
     @GetMapping
-    public String getPost() {
-        return "Post got";
+    public ResponseEntity<String> getPost() {
+        return ResponseEntity.status(HttpStatus.OK).body("Post got");
     }
 
     @PutMapping
-    public String putPost() {
-        return "Post updated";
+    public ResponseEntity<String> putPost() {
+        return ResponseEntity.status(HttpStatus.OK).body("Post updated");
     }
 
     @DeleteMapping
-    public String delPost() {
-        return "Post deleted";
+    public ResponseEntity<String> delPost() {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product deleted");
     }
 }
